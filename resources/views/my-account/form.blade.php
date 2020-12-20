cf8496cd79c156479a807f5731f6f9610e998440
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2020-12-19 21:10:35 -0300

    ajustes

--- resources/views/my-account/form.blade.php
+++ resources/views/my-account/form.blade.php
@@ -21,14 +21,14 @@
 				label="Nova senha"
 				v-model="form.password"
 				type="password"
-				autocomplete="no"
+				autocomplete="chrome-off"
 				:error-message="form.errors.get('password')"></md-input>
 
 			<md-input class="col mb-1"
 				name="password_confirmation"
 				label="Confirme a senha"
 				type="password"
-				autocomplete="no"
+				autocomplete="chrome-off"
 				v-model="form.password_confirmation"></md-input>
 		</div>
 
